47154b576dd14365cc0eacb6ed18518225289790
Aaron Aguerrevere <aguerrevere@legacy.com>
2020-12-18 16:24:50 -0600

    imported several functions from Util, min and max

---
+++ aboveaverage.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my @fred = qw{ 1 3 5 7 9 };
+
+sub above_average {
+
+}
--- count.pl
+++ count.pl
@@ -1,13 +1,17 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use List::Util qw(sum);
+use List::Util qw(min max sum product);
+# minstr & minstr are similar, tho with ASCII order
+# https://metacpan.org/pod/List::Util 
 
 my @nums = ( 1 .. 1000);
 
-sub total {
-    return sum(@nums);
+sub utils {
+    print "Smallest number: " . min(@nums) . ' ';
+    print "Biggest number: " . max(@nums) . ' ';
+    print "Sum of all numbers in range: " . sum(@nums) . ' ';
 }
 
-my $nums_total = total(@nums);
-print $nums_total;
+my $nums_totals = utils(@nums);
+print $nums_totals;
