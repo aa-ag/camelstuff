7b721b16262f7e2c42a4e025a5a56e6355e9cf9f
Aaron Aguerrevere <aguerrevere@legacy.com>
2020-12-21 13:11:54 -0600

    keys and values Functions

--- hashes.pl
+++ hashes.pl
@@ -27,6 +27,12 @@ my %last_name = (
     'betty' => 'rubble',
 );
 
-while ( ($key, $value) = each %last_name ) {
-    print "$key => $value\n";
-}
+# while ( ($key, $value) = each %last_name ) {
+#     print "$key => $value\n";
+# }
+
+my %hash = ('a' => 1, 'b' => 2, 'c' => 3);
+my @k = keys %hash;
+my @v = values %hash;
+
+print @k, @v
