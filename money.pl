1279f590bea658d679cbf06d297b8397eef946d6
aa-ag <aaron.aguerrevere@gmail.com>
2020-12-26 17:02:46 -0600

    formatting numbers

---
+++ money.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+my $money = sprintf "%.2f", 2.49997;
+print $money;
+# 2.50                                                                                                        
+
+sub big_money {
+    my $number = sprintf "%.2f", shift @_;
+    1 while $number =~ s/^(-?\d+)(\d\d\d)/$1,$2/;
+    $number =~ s/^(-?)/$1\$/;
+    $number;
+}
+
+print big_money(12345678.9);
+# $12,345,678.90
