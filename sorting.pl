d992e045f06e23f1372c112542ff0b0da663f30f
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-01 17:55:38 -0600

    sorted numerical array

--- sorting.pl
+++ sorting.pl
@@ -10,3 +10,11 @@ sub by_score { $score{$b} <=> $score{$a} };
 
 my @winners = sort by_score keys %score;
 print(@winners);
+# fredbarneydino                                                                                              
+
+# 1.
+my @numbers = (17, 1000, 04, 1.50, 3.14159, -10, 1.5, 4, 2001, 90210, 666);
+
+@sorted = sort { $a <=> $b } @numbers; 
+print "sorted array is: @sorted";
+# sorted array is: -10 1.5 1.5 3.14159 4 4 17 666 1000 2001 90210
