068b42380e556652fa8035d77592dede572736b3
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-01 18:01:45 -0600

    search for substrings working

--- sorting.pl
+++ sorting.pl
@@ -18,3 +18,25 @@ my @numbers = (17, 1000, 04, 1.50, 3.14159, -10, 1.5, 4, 2001, 90210, 666);
 @sorted = sort { $a <=> $b } @numbers; 
 print "sorted array is: @sorted";
 # sorted array is: -10 1.5 1.5 3.14159 4 4 17 666 1000 2001 90210
+
+# 3.
+print "please enter a string: ";
+chomp(my $string = <STDIN>);
+print "please enter a substring: ";
+chomp(my $substr = <STDIN>);
+
+my @indices;
+
+for (my $pos = -1; ;) {
+    $pos = index($string, $substr, $pos + 1);
+    last if $pos == -1;
+    push @indices, $pos;
+}
+
+print "Indices: @indices."
+
+'''
+this this this is a string
+this
+0 5 1
+'''
